0ad95fe84ae3daf8a806bbd90f7efbb69bb94e7e
nickbron <77615583+nickbron@users.noreply.github.com>
2021-12-05 23:09:52 +0200

    create

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,16 @@
 const { Contact } = require("../../models");
+const { Conflict } = require("http-errors");
 
 const addContact = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { name } = req.body;
+  const user = await Contact.findOne({ name });
+  console.log(user);
+  if (user) {
+    throw new Conflict(`User with ${name} already exist`);
+  }
+
+  const { _id } = req.user;
+  const result = await Contact.create({ ...req.body, owner: _id });
   res.status(201).json({
     status: "success",
     code: 201,
