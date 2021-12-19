f76a40be527d8ca426f4d17ee408c00a55c27a7a
nickbron <77615583+nickbron@users.noreply.github.com>
2021-12-19 13:17:51 +0200

    create

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,5 +1,13 @@
 const getCurrent = require("./getCurrent");
 const updateSubscription = require("./updateSubscription");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const verifyResend = require("./verifyResend");
 
-module.exports = { getCurrent, updateSubscription, updateAvatar };
+module.exports = {
+  getCurrent,
+  updateSubscription,
+  updateAvatar,
+  verifyEmail,
+  verifyResend,
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ const { users: ctrl } = require("../../controllers");
 const router = express.Router();
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
+router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail));
+router.post("/verify", ctrlWrapper(ctrl.verifyResend));
 router.patch("/", auth, ctrlWrapper(ctrl.updateSubscription));
 router.patch(
   "/avatars",
